9b404c825c7d59d6565d32864c3050f386701c90
Alex <djdoktop@gmail.com>
2018-02-26 01:30:16 +0300

    almost

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
-}
+    let res = 0;
+    let i = 1;
+    while(Math.pow(5,i)<=number){
+        res+=Math.round(number/Math.pow(5,i));
+        i++;
+    }
+    return res;
+};// До какой точки?
