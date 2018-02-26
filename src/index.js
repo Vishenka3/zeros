ecdc108fe920177a666bf61e1f4993e0820f8d27
Alex <djdoktop@gmail.com>
2018-02-26 12:38:17 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ module.exports = function getZerosCount(number) {
   // your implementation
     let res = 0;
     let i = 1;
-    while(Math.pow(5,i)<=number){
-        res+=Math.round(number/Math.pow(5,i));
+    while(Math.pow(5,i)<number){
+        res+=Math.floor(number/Math.pow(5,i));
         i++;
     }
     return res;
-};// До какой точки?
+};
+
